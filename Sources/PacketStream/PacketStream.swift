2340145ec9c547f23aeb0464a3b230f6afaa529b
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2020-12-30 13:37:48 -0600

    Public inits

--- Sources/PacketStream/PacketStream.swift
+++ Sources/PacketStream/PacketStream.swift
@@ -4,12 +4,24 @@ public struct TimestampedPacket
 {
     public let timestamp: Date
     public let payload: Data
+
+    public init(timestamp: Date, payload: Data)
+    {
+        self.timestamp = timestamp
+        self.payload = payload
+    }
 }
 
 public struct CaptureResult
 {
     public let packets: [TimestampedPacket]
     public let dropped: Int
+
+    public init(packets: [TimestampedPacket], dropped: Int)
+    {
+        self.packets = packets
+        self.dropped = dropped
+    }
 }
 
 public protocol PacketStream
