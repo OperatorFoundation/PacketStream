dce0f52927bbdac2a83ca5cbe977b5aaf2cc877f
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2020-12-30 12:54:53 -0600

    Changed interface

--- Sources/PacketStream/PacketStream.swift
+++ Sources/PacketStream/PacketStream.swift
@@ -1,6 +1,20 @@
 import Foundation
 
+public struct TimestampedPacket
+{
+    public let timestamp: Date
+    public let payload: Data
+}
+
+public struct CaptureResult
+{
+    public let packets: [TimestampedPacket]
+    public let dropped: Int
+}
+
 public protocol PacketStream
 {
-    func nextPacket() -> (Date, Data)
+    func startCapture()
+    func nextCaptureResult() -> CaptureResult?
+    func stopCapture()
 }
